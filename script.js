39df6cabf72bbd6b0ac82021fbf963371e33607a
Henry <siyu.han2805@gmail.com>
2024-06-17 17:28:56 +1000

    Complete Etch-a-sketch project

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//global variable containing current colour 
+let currentClr = "black";
 
 //selecting the slider 
 const slider = document.querySelector("input");
@@ -5,6 +7,12 @@ const slider = document.querySelector("input");
 const sliderLabel = document.querySelector("#slider-label");
 //selecting the game-sketch section
 const sketchArea = document.querySelector(".game-sketch");
+//selecting the "clear" button
+const clearBtn = document.querySelector(".btn-clear");
+//selectio
+const clrBtns = document.querySelectorAll(".clr-btn");
+const clrSettings = document.querySelector(".btn-colours");
+const clrSection = document.querySelector("aside");
 
 
 //function to reflect slider value on screen
@@ -43,7 +51,7 @@ const createPixel = (width, height, num) => {
         element.style.height = `${height}px`;
         element.classList.add("pixel");
         element.addEventListener("mouseover", () => {
-            element.classList.add("pixel-hover");
+            element.style.backgroundColor = currentClr;
         })
         sketchArea.appendChild(element);
     }
@@ -57,8 +65,63 @@ const clearGrid = () => {
     });
 }
 
+//function to clear dark pixels after "clear" button is pressed
+const clearDarkGrid = () => {
+    const darkPixels = document.querySelectorAll(".pixel");
+    darkPixels.forEach(pixel => {
+        pixel.style.removeProperty("background-color");
+    })
+}
+
+//function to make the opacity of btns as "unselected" so user can select new btn
+const clearBtns = () => {
+    clrBtns.forEach(btn => {
+        btn.style.opacity = "0.7";
+    })
+}
+
 //initial grid generation
 gridGenerator();
 
 //listen for slider movements and reflect in sliderLabel
 slider.addEventListener("input", onSliderInput);
+//listen to clear the board
+clearBtn.addEventListener("click", clearDarkGrid);
+//listen to dropdown colour picker
+clrSettings.addEventListener("click", () => {
+    if (clrSection.classList.contains("hidden")) {
+        clrSection.classList.remove("hidden");
+        clrSection.classList.add("visible");
+        clrSection.style.display = "flex";
+        clrSection.style.animation = "settingsSlideDown 1000ms linear 0s 1 normal";
+        setTimeout(() => {
+            clrSection.style.zIndex = 1;
+        }, 1000);
+        
+        
+    }
+    else if (clrSection.classList.contains("visible")) {
+        clrSection.classList.remove("visible");
+        clrSection.classList.add("hidden");
+        clrSection.style.animation = "settingsSlideUp 1000ms linear 0s 1 normal";
+        clrSection.style.zIndex = -1;
+        setTimeout(() => {
+            clrSection.style.display = "none"
+        }, 1000);
+    }
+})
+
+
+//add click listeners to each colour btn for colour change
+clrBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        //clear selection visual of other buttons
+        clearBtns();
+        //add the style of opacity change
+        btn.style.opacity = "1";
+        //set currentClr global var to the colour value
+        const computedStyle = window.getComputedStyle(e.target);
+        currentClr= computedStyle.backgroundColor;
+    })
+})
+
